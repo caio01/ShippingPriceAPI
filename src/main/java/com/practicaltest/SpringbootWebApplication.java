c709c42092557735ff53d2e05ca0d00675388f2c
Caio Oliveira <caioinacio00@gmail.com>
2023-03-11 15:33:28 -0300

    create project

---
+++ src/main/java/com/practicaltest/SpringbootWebApplication.java
@@ -0,0 +1,15 @@
+package com.practicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringbootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebApplication.class, args);
+    }
+
+}
